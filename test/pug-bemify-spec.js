a6d7ef9cb31331b16e569fd24f927c31bea96c07
Weblee Dev <dev@weblee.co>
2019-04-08 00:54:24 -0400

    improves specs structure * adds instanbul for code coverage

--- test/pug-bemify-spec.js
+++ test/pug-bemify-spec.js
@@ -3,78 +3,71 @@ const pug = require('pug'),
 
     expect = chai.expect;
 
-describe('PugLexBemifier', function () {
+describe('PugBEMAST', function () {
     const pugBEMLexer = require('../');
 
-    it('can edit the default pug tokens', function () {
-        const testPugTxt = `.block.__child Hello World`;
+    describe("toPugAST()", function () {
 
-        const result = pug.render(testPugTxt, {
-            plugins: [pugBEMLexer()]
+        it('should be able to parse the bare minimum pug text', function () {
+            const testPugTxt = `.block.__child`;
+
+            const result = pug.render(testPugTxt, {
+                plugins: [pugBEMLexer()]
+            });
+
+            expect(result).to.eql('<div class="block block__child"></div>');
         });
 
-        expect(result).to.eql('<div class="block block__child">Hello World</div>');
-    });
+        it('should modify multiple shorthand class names', function () {
+            const testPugTxt = `.block.--modA.--modB`;
 
-    it('can parse the bare minimum pug text', function () {
-        const testPugTxt = `.block.__child`;
+            const result = pug.render(testPugTxt, {
+                plugins: [pugBEMLexer()]
+            });
 
-        const result = pug.render(testPugTxt, {
-            plugins: [pugBEMLexer()]
+            expect(result).to.eql('<div class="block block--modA block--modB"></div>');
         });
 
-        expect(result).to.eql('<div class="block block__child"></div>');
-    });
+        it('should parse multiple lines', function () {
+            const testPugTxt = `.block.__child\n\t.grand-child.__test Hello World`,
+                expectedResulted = '<div class="block block__child"><div class="grand-child grand-child__test">Hello World</div></div>';
 
-    it('can modify multiple shorthand class names', function () {
-        const testPugTxt = `.block.--modA.--modB`;
+            const result = pug.render(testPugTxt, {
+                plugins: [pugBEMLexer()]
+            });
 
-        const result = pug.render(testPugTxt, {
-            plugins: [pugBEMLexer()]
+            expect(result).to.eql(expectedResulted);
         });
 
-        expect(result).to.eql('<div class="block block--modA block--modB"></div>');
-    });
+        it('should handle multiple nested blocks', function () {
+            const testPugTxt = ".block" +
+                "\n\t.__parent" +
+                "\n\t\t.__child" +
+                "\n\t\t\t.__test_child" +
+                "\n\t\t\t.aGranChild" +
+                "\n\t\t\t\t.__child" +
+                "\n\t\t.__chlid_sibling" +
+                "\n\t\t\t.__chlid_sibling_child",
 
-    it('can parse multiple lines', function () {
-        const testPugTxt = `.block.__child\n\t.grand-child.__test Hello World`,
-            expectedResulted = '<div class="block block__child"><div class="grand-child grand-child__test">Hello World</div></div>';
+                expectedResulted = '<div class="block">' +
+                    '<div class="block__parent">' +
+                    '<div class="block__child">' +
+                    '<div class="block__test_child"></div>' +
+                    '<div class="aGranChild">' +
+                    '<div class="aGranChild__child"></div>' +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="block__chlid_sibling">' +
+                    '<div class="block__chlid_sibling_child"></div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
 
-        const result = pug.render(testPugTxt, {
-            plugins: [pugBEMLexer()]
-        });
+            const result = pug.render(testPugTxt, {
+                plugins: [pugBEMLexer()]
+            });
 
-        expect(result).to.eql(expectedResulted);
-    });
-
-    it('can correctly parse multiple nested blocks', function () {
-        const testPugTxt = ".block" +
-          "\n\t.__parent" +
-          "\n\t\t.__child" +
-          "\n\t\t\t.__test_child" +
-          "\n\t\t\t.aGranChild" +
-          "\n\t\t\t\t.__child" +
-          "\n\t\t.__chlid_sibling" +
-          "\n\t\t\t.__chlid_sibling_child",
-
-          expectedResulted = '<div class="block">' +
-              '<div class="block__parent">' +
-                '<div class="block__child">' +
-                  '<div class="block__test_child"></div>' +
-                  '<div class="aGranChild">' +
-                  '<div class="aGranChild__child"></div>' +
-                  '</div>' +
-                '</div>' +
-                '<div class="block__chlid_sibling">' +
-                  '<div class="block__chlid_sibling_child"></div>' +
-                '</div>' +
-              '</div>' +
-            '</div>';
-
-        const result = pug.render(testPugTxt, {
-            plugins: [pugBEMLexer()]
-        });
-
-        expect(result).to.eql(expectedResulted);
+            expect(result).to.eql(expectedResulted);
+        })
     })
 });
